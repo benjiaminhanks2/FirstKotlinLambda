2dab02b31793e4dc59695a1847244399f23840c0
Sahil Jam <sahiljambhekar@github.com>
2018-06-12 00:55:17 -0400

    Add com.spark Webserver dependency with /hello endpoint.

--- src/main/kotlin/org/jam/kotlin/Hello.kt
+++ src/main/kotlin/org/jam/kotlin/Hello.kt
@@ -1,6 +1,10 @@
 package org.jam.kotlin
 
+import spark.Spark.get
+import org.apache.log4j.BasicConfigurator
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+    BasicConfigurator.configure()
+    get("/hello") {req,res -> "Hello World"}
 }
 
